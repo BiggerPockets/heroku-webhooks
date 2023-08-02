8b298ec6b63a6dcaeb8ef216e67732cc13fee9c1
John Gallagher <johngallagher@biggerpockets.com>
2023-08-02 17:05:33 +0100

    fix: skip CSRF verification

--- app/controllers/invalid_user_ids_controller.rb
+++ app/controllers/invalid_user_ids_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class InvalidUserIdsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def update
     InvalidUserId.find_by!(value: params[:id]).update!(aliased_to: params[:aliased_to])
     head :ok
